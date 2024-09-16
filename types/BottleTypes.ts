8215e92fc838e2becaaab84c68f2508bd6f5bd22
faizanali1192 <faizanit1192@gmail.com>
2024-09-16 19:41:51 +0500

    add bottles module types

--- types/BottleTypes.ts
+++ types/BottleTypes.ts
@@ -1,7 +1,53 @@
-export interface BottleListParamTypes {
-  page?: number;
-  pageLimit?: number;
-  sortDirection?: "asc" | "desc";
+import { paginationParamsType, PaginationResponseType } from "./CustomerTypes";
+
+export interface BottlesPaginationParamType
+  extends Omit<paginationParamsType, "search" | "orderBy"> {}
+
+export interface BottleListParamTypes extends BottlesPaginationParamType {
   startDate: Date;
   endDate: Date;
+}
+export interface BottlesModelResponseType {
+  bottleId: number;
+  customerId: number;
+  deliveredBottlesQuantity: number;
+  receivedBottlesQuantity: number;
+  deliveryDate: string;
+  verifiedByCustomer: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface GetAllBottlesResponseType {
+  status: string;
+  data: BottlesModelResponseType[];
+  pagination: BottlesPaginationParamType;
+}
+
+export interface BottleRequestTypes
+  extends Pick<
+    BottlesModelResponseType,
+    | "deliveredBottlesQuantity"
+    | "receivedBottlesQuantity"
+    | "deliveryDate"
+    | "verifiedByCustomer"
+  > {}
+
+export interface BottleResponseType {
+  status: string;
+  data: BottlesModelResponseType;
+}
+export interface AddBottleParamType {
+  customerId: string;
+}
+
+export interface UpdateBottleParamType extends AddBottleParamType {
+  bottleId: string;
+}
+
+export interface DeleteBottleParamType extends UpdateBottleParamType {}
+
+export interface DeleteBottleResponseType
+  extends Omit<BottleResponseType, "data"> {
+  data: string;
 }
