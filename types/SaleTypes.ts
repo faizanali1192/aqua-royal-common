460ac7e61f8c4735851e01c66d0839b87e326851
faizanali1192 <faizanit1192@gmail.com>
2025-04-06 14:35:45 +0500

    sale types added

---
+++ types/SaleTypes.ts
@@ -0,0 +1,40 @@
+export interface CreateSaleRequest {
+  saleDesc?: string;
+  saleAmount: number;
+  saleDate: Date | string;
+}
+
+export interface CreateSaleResponse {
+  status: "success" | "error";
+  data?: any;
+  message?: string;
+}
+
+export interface UpdateSaleRequest {
+  saleDesc?: string;
+  saleAmount: number;
+  saleDate: Date | string;
+}
+
+export interface UpdateSaleResponse {
+  status: "success" | "error";
+  data?: any;
+  message?: string;
+}
+
+export interface DeleteSaleResponse {
+  status: "success" | "error";
+  message: string;
+}
+
+export interface GetSalesResponseType {
+  status: "success" | "error";
+  data: any[];
+  totalSaleAmount: number;
+  pagination: {
+    totalItems: number;
+    totalPages: number;
+    currentPage: number;
+    pageSize: number;
+  };
+}
